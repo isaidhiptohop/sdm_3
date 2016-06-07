00b00a6d278aba337dffa9eed8765d37907bbf6e
Jakob Moosbrugger <jak.moo54@gmail.com>
2016-06-07 17:34:45 +0200

    lsh kmeans

---
+++ src/at/ac/univie/PRG3/Bucket.java
@@ -0,0 +1,14 @@
+package at.ac.univie.PRG3;
+
+/**
+ * Created by Jakob on 07.06.2016.
+ */
+public class Bucket {
+    public double max;
+    public int cluster;
+
+    public Bucket(double max, int cluster) {
+        this.max = max;
+        this.cluster = cluster;
+    }
+}
--- src/at/ac/univie/PRG3/Main.java
+++ src/at/ac/univie/PRG3/Main.java
@@ -15,6 +15,16 @@ public class Main {
     int points;
     final int clusters = 15;
     final int dimension = 10;
+    Bucket[][] centroidHashes;
+    int dataHash[];
+    int[] clusterHashes;
+
+    double bucketSize = 10;
+//    final double threshold = 100;
+
+    final int funcGroups = 4;
+    final int funcs = funcGroups * funcGroups;
+    double[] fHash;
 
     public void loadData(String path) {
         try {
@@ -26,6 +36,16 @@ public class Main {
             points = sp.length /dimension;
             clusterIDs = new int[points];
             numClusterPoints = new int[clusters];
+            fHash = new double[funcs * dimension];
+            centroidHashes = new Bucket[funcs][clusters];
+            dataHash = new int[funcs*points];
+            clusterHashes = new int[funcs * clusters];
+
+            for(int i = 0; i < funcs; ++i) {
+                for(int j = 0; j < clusters; ++j) {
+                    centroidHashes[i][j] = new Bucket(-1,-1);
+                }
+            }
 
 
             double min = Double.MAX_VALUE, max = -Double.MAX_VALUE;
@@ -44,6 +64,11 @@ public class Main {
             for(int i = 0; i < clusters * dimension; ++i) {
                 centroids[i] = rng.nextDouble() * (max - min) + min;
             }
+
+            for(int i = 0; i < funcs * dimension; ++i) {
+                fHash[i] = (rng.nextDouble()-0.5) *2;
+            }
+
 
         } catch (Exception e) {
             System.err.println("can't read file.");
@@ -135,6 +160,121 @@ public class Main {
         }
     }
 
+    private int hash(int point, boolean isData, int func) {
+        double hashed = 0;
+        if(isData) {
+            for(int i = 0; i < dimension; ++i) {
+                hashed += data[point + i] * fHash[func * dimension + i];
+            }
+        } else {
+            for(int i = 0; i < dimension; ++i) {
+                hashed += centroids[point + i] * fHash[func * dimension + i];
+            }
+        }
+        hashed = hashed / bucketSize;
+
+        return (int)Math.floor(hashed);
+    }
+
+    private void kmeansLsh() {
+        double minDist = Double.MAX_VALUE, tmp;
+        int clusterID = -1, changed;
+        boolean converged = false;
+
+        for(int i = 0; i < tmpCentroids.length; ++i) {
+            tmpCentroids[i] = 0;
+        }
+
+        for(int i = 0; i < points; ++i) {
+            for(int j = 0; j < funcs; ++j) {
+                dataHash[i * funcs + j] = hash(i * dimension, true, j);
+            }
+        }
+
+        while (!converged) {
+            converged = true;
+            changed = 0;
+
+            for(int i = 0; i < numClusterPoints.length; ++i)
+                numClusterPoints[i] = 0;
+
+            // new ...
+            for(int i = 0; i < clusters; ++i) {
+                for (int j = 0; j < funcs; ++j) {
+                    clusterHashes[i*funcs + j] = hash(i * dimension, false, j);
+                }
+            }
+
+            // ... till here
+
+            for (int i = 0; i < points; ++i) {
+                // new ...
+                clusterID = -1;
+                for (int j = 0; j < funcs; ++j) {
+                    dataHash[j] = hash(i * dimension, true, j);
+                    for(int k = 0; k < clusters; ++k) {
+                        if(dataHash[j] < centroidHashes[j][k].max) {
+                            //System.out.println(dataHashV[j] + " : " + centroidHashes[j][k].max);
+                            if(clusterID == -1) {
+                                clusterID = centroidHashes[j][k].cluster;
+                            } else if(clusterID != centroidHashes[j][k].cluster) { //if in func group different cluster
+                                j = ((j/funcGroups) + 1) * funcGroups; //set to begin of next func group
+                                clusterID = -1;
+                                if(j >= funcs)
+                                    break;
+                                continue;
+                            } else if((j + 1) % funcGroups == 0) { //if full func group is passed
+                                break; //abort calc
+                            }
+                        }
+                    }
+                }
+
+
+
+/*                minDist = Math.abs(centroidHashes[0] - dataHashV);
+                for (int j = 1; j < clusters; ++j) {
+                    tmp = Math.abs(centroidHashes[j] - dataHashV);
+                    if(tmp < minDist) {
+                        minDist = tmp;
+                        clusterID = j;
+                    }
+                }
+
+                minDist = distance(i * dimension, clusterID * dimension);
+                // ... till here
+*/
+
+
+//                if (minDist < distance(i * dimension, clusterIDs[i] * dimension)) { // new
+
+                    if (clusterIDs[i] != clusterID) {
+                        converged = false;
+                        clusterIDs[i] = clusterID;
+                        ++changed;
+                    }
+
+//                } // new
+
+                if(clusterID != -1) {
+                    for (int k = 0; k < dimension; ++k) {
+                        tmpCentroids[clusterID * dimension + k] += data[i * dimension + k];
+                    }
+                    ++numClusterPoints[clusterID];
+                }
+            }
+
+
+            for(int i = 0; i < tmpCentroids.length; ++i) {
+                if(numClusterPoints[i/dimension] != 0) {
+                    centroids[i] = tmpCentroids[i] / numClusterPoints[i / dimension];
+                    tmpCentroids[i] = 0;
+                }
+            }
+            System.out.println("changed: " + changed);
+        }
+    }
+
     public static void main(String[] args) {
         Main prg = new Main();
         prg.run(args);
@@ -157,7 +297,7 @@ public class Main {
 
         System.out.println("calculation started (n = " + points + ")");
         start = System.nanoTime();
-        kmeans();
+        kmeansLsh();
         end = System.nanoTime();
 
         long duration = end - start;
